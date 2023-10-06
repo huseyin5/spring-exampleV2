48757e83e7587bb8f731cf05805f736167af3e09
huseyin <husseyn.mutl@gmail.com>
2023-10-06 09:15:08 +0300

    Initial commit

---
+++ spring-datajpa-postgresql/src/main/java/com/haydikodlayalim/dto/KisiDto.java
@@ -0,0 +1,13 @@
+package com.haydikodlayalim.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class KisiDto {
+    private Long id;
+    private String adi;
+    private String soyadi;
+    private List<String> adresler;
+}
---
+++ spring-datajpa-postgresql/src/test/java/com/haydikodlayalim/springdatajpapostgresql/SpringDatajpaPostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haydikodlayalim.springdatajpapostgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDatajpaPostgresqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
