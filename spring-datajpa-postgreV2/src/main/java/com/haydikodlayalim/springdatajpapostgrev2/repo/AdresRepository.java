09b0284a27c35a2995118f22c9f8391bfe166020
huseyin <husseyn.mutl@gmail.com>
2023-10-09 09:54:50 +0300

    update

--- spring-datajpa-postgreV2/src/main/java/com/haydikodlayalim/springdatajpapostgrev2/repo/AdresRepository.java
+++
@@ -1,7 +0,0 @@
-package com.haydikodlayalim.springdatajpapostgrev2.repo;
-
-import com.haydikodlayalim.springdatajpapostgresql.entitiy.Adres;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdresRepository extends JpaRepository<Adres, Long> {
-}
