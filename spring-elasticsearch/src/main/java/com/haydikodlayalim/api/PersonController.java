c898336faec7369bf2809fb038c65bd78c44d168
huseyin <husseyn.mutl@gmail.com>
2023-10-11 15:51:54 +0300

    update

--- spring-elasticsearch/src/main/java/com/haydikodlayalim/api/PersonController.java
+++ spring-elasticsearch/src/main/java/com/haydikodlayalim/api/PersonController.java
@@ -5,6 +5,7 @@ import com.haydikodlayalim.repository.PersonRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,8 +30,11 @@ public class PersonController {
         personRepository.save(person);
     }
 
+    @GetMapping("/{search}")
     public ResponseEntity<List<Person>> getPerson(@PathVariable String search) {
         List<Person> persons = personRepository.findByNameLikeOrLastNameLike(search, search);
         return ResponseEntity.ok(persons);
     }
+
+
 }
