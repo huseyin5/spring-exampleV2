de1a0a679f191c43f09d4932e6522763ea19053c
huseyin <husseyn.mutl@gmail.com>
2023-10-16 13:57:21 +0300

    fixed graphql-server

--- springboot-graphql/src/main/java/com/example/graphqlserver/BookController.java
+++ springboot-graphql/src/main/java/com/example/graphqlserver/BookController.java
@@ -2,6 +2,7 @@ package com.example.graphqlserver;
 
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -12,6 +13,7 @@ public class BookController {
         return Book.getById(id);
     }
 
+    @SchemaMapping
     public Author author(Book book) {
         return Author.getById(book.authorId());
     }
